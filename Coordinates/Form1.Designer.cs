9f19655edff6b80d53d6f694762b58cf4f4275d5
hoangmanhkhiem <khiemhm04@gmail.com>
2024-08-27 09:56:26 +0700

    Add project files.

---
+++ Coordinates/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace Coordinates
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.x_1 = new System.Windows.Forms.TextBox();
+            this.y_1 = new System.Windows.Forms.TextBox();
+            this.x_2 = new System.Windows.Forms.TextBox();
+            this.y_2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.buttonresult = new System.Windows.Forms.Button();
+            this.resultdistance = new System.Windows.Forms.TextBox();
+            this.resulta = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // x_1
+            // 
+            this.x_1.AccessibleDescription = "";
+            this.x_1.AccessibleName = "";
+            this.x_1.Location = new System.Drawing.Point(76, 89);
+            this.x_1.Name = "x_1";
+            this.x_1.Size = new System.Drawing.Size(100, 20);
+            this.x_1.TabIndex = 0;
+            this.x_1.Text = "Nhập x1";
+            // 
+            // y_1
+            // 
+            this.y_1.Location = new System.Drawing.Point(76, 115);
+            this.y_1.Name = "y_1";
+            this.y_1.Size = new System.Drawing.Size(100, 20);
+            this.y_1.TabIndex = 1;
+            this.y_1.Text = "Nhập y1";
+            // 
+            // x_2
+            // 
+            this.x_2.Location = new System.Drawing.Point(77, 177);
+            this.x_2.Name = "x_2";
+            this.x_2.Size = new System.Drawing.Size(100, 20);
+            this.x_2.TabIndex = 2;
+            this.x_2.Text = "Nhập x2";
+            // 
+            // y_2
+            // 
+            this.y_2.Location = new System.Drawing.Point(77, 203);
+            this.y_2.Name = "y_2";
+            this.y_2.Size = new System.Drawing.Size(100, 20);
+            this.y_2.TabIndex = 3;
+            this.y_2.Text = "Nhập y2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Điểm thứ nhất";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "tọa độ x";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "tọa độ y";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "tọa độ y";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 180);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "tọa độ x";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(97, 158);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Điểm thứ hai";
+            // 
+            // buttonresult
+            // 
+            this.buttonresult.Location = new System.Drawing.Point(12, 242);
+            this.buttonresult.Name = "buttonresult";
+            this.buttonresult.Size = new System.Drawing.Size(187, 23);
+            this.buttonresult.TabIndex = 10;
+            this.buttonresult.Text = "Tính toán";
+            this.buttonresult.UseVisualStyleBackColor = true;
+            this.buttonresult.Click += new System.EventHandler(this.buttonresult_Click);
+            // 
+            // resultdistance
+            // 
+            this.resultdistance.Location = new System.Drawing.Point(12, 335);
+            this.resultdistance.Name = "resultdistance";
+            this.resultdistance.Size = new System.Drawing.Size(187, 20);
+            this.resultdistance.TabIndex = 11;
+            // 
+            // resulta
+            // 
+            this.resulta.Location = new System.Drawing.Point(12, 412);
+            this.resulta.Name = "resulta";
+            this.resulta.Size = new System.Drawing.Size(187, 20);
+            this.resulta.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(73, 310);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(71, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Khoảng cách";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(80, 393);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(56, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Hệ số góc";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(69, 454);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(769, 454);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(27, 23);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(769, 489);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(27, 23);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox2.Location = new System.Drawing.Point(226, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(574, 521);
+            this.pictureBox2.TabIndex = 17;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(232, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(532, 479);
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(39, 513);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(138, 13);
+            this.linkLabel1.TabIndex = 22;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Author: Hoàng Mạnh Khiêm";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(2, 26);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(218, 16);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "COORDINATES APPLICATION";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 545);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.resulta);
+            this.Controls.Add(this.resultdistance);
+            this.Controls.Add(this.buttonresult);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.y_2);
+            this.Controls.Add(this.x_2);
+            this.Controls.Add(this.y_1);
+            this.Controls.Add(this.x_1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox x_1;
+        private System.Windows.Forms.TextBox y_1;
+        private System.Windows.Forms.TextBox x_2;
+        private System.Windows.Forms.TextBox y_2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button buttonresult;
+        private System.Windows.Forms.TextBox resultdistance;
+        private System.Windows.Forms.TextBox resulta;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Coordinates/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+//Thiết kế chương trình để nhập vào tọa độ của 2 điểm (x1, y1) và (x2, y2) và tính:
+//a, Hệ số góc của đường thẳng đi qua 2 điểm đó theo công thức: 61,1 925 70->1170
+//     Hệ số góc=(y2-y1)/( x2-x1)
+//b, Khoảng cách giữa 2 điểm
+
+
+namespace Coordinates
+{
+    public partial class Form1 : Form
+    {
+        private float zoomFactor = 1.0f;
+        private const float zoomStep = 0.1f;
+        public Form1()
+        {
+            InitializeComponent();         
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonresult_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double x1 = double.Parse(x_1.Text);
+                if (x1 is double.NaN)
+                {
+                    throw new Exception("Nhập sai kiểu dữ liệu x1");
+                }
+                double y1 = double.Parse(y_1.Text);
+                if (y1 is double.NaN)
+                {
+                    throw new Exception("Nhập sai kiểu dữ liệu y1");
+                }
+                double x2 = double.Parse(x_2.Text);
+                if (x2 is double.NaN)
+                {
+                    throw new Exception("Nhập sai kiểu dữ liệu x2");
+                }
+                double y2 = double.Parse(y_2.Text);
+                if (y2 is double.NaN)
+                {
+                    throw new Exception("Nhập sai kiểu dữ liệu y2");
+                }
+                double a = (y2 - y1) / (x2 - x1);
+                double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+                resulta.Text = a.ToString();
+                resultdistance.Text = distance.ToString();
+                pictureBox1.Invalidate();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có muốn thoát không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+                e.Cancel = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            x_1.Text = "";
+            y_1.Text = "";
+            x_2.Text = "";
+            y_2.Text = "";
+            resulta.Text = "";
+            resultdistance.Text = "";
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen pen = new Pen(Color.Red, 2);
+            Font font = new Font("Arial", 8);
+
+            if (double.TryParse(x_1.Text, out double x1) &&
+                double.TryParse(y_1.Text, out double y1) &&
+                double.TryParse(x_2.Text, out double x2) &&
+                double.TryParse(y_2.Text, out double y2))
+            {
+                int startX = pictureBox1.Width / 2 + (int)(x1 * 20 * zoomFactor);
+                int startY = pictureBox1.Height / 2 - (int)(y1 * 20 * zoomFactor);
+                int endX = pictureBox1.Width / 2 + (int)(x2 * 20 * zoomFactor);
+                int endY = pictureBox1.Height / 2 - (int)(y2 * 20 * zoomFactor);
+
+                g.DrawLine(pen, 0, pictureBox1.Height / 2, pictureBox1.Width, pictureBox1.Height / 2);
+                g.DrawLine(pen, pictureBox1.Width / 2, 0, pictureBox1.Width / 2, pictureBox1.Height);
+
+                for (int i = 1; i < pictureBox1.Width / (20 * zoomFactor); i++)
+                {
+                    int x = pictureBox1.Width / 2 + (int)(i * 20 * zoomFactor);
+                    int y = pictureBox1.Height / 2;
+                    g.DrawLine(pen, x, y - 5, x, y + 5);
+                    g.DrawString((i).ToString(), font, Brushes.Black, x + 2, y + 5);
+                    g.DrawLine(pen, pictureBox1.Width / 2 - (int)(i * 20 * zoomFactor), y - 5, pictureBox1.Width / 2 - (int)(i * 20 * zoomFactor), y + 5);
+                    g.DrawString((-i).ToString(), font, Brushes.Black, pictureBox1.Width / 2 - (int)(i * 20 * zoomFactor) + 2, y + 5);
+                }
+
+                for (int i = 1; i < pictureBox1.Height / (20 * zoomFactor); i++)
+                {
+                    int x = pictureBox1.Width / 2;
+                    int y = pictureBox1.Height / 2 + (int)(i * 20 * zoomFactor);
+                    g.DrawLine(pen, x - 5, y, x + 5, y);
+                    g.DrawString((i).ToString(), font, Brushes.Black, x - 20, y + 2);
+                    g.DrawLine(pen, x - 5, pictureBox1.Height / 2 - (int)(i * 20 * zoomFactor), x + 5, pictureBox1.Height / 2 - (int)(i * 20 * zoomFactor));
+                    g.DrawString((-i).ToString(), font, Brushes.Black, x - 20, pictureBox1.Height / 2 - (int)(i * 20 * zoomFactor) + 2);
+                }
+
+                if (x1 == x2)
+                {
+                    MessageBox.Show("Hai điểm có cùng giá trị x. Không thể vẽ đường thẳng.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    g.FillEllipse(Brushes.Blue, startX - 2, startY - 2, 4, 4);
+                    g.FillEllipse(Brushes.Blue, endX - 2, endY - 2, 4, 4);
+                    g.DrawLine(pen, startX, startY, endX, endY);
+                }
+            }
+
+            pen.Dispose();
+            font.Dispose();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            zoomFactor += zoomStep;
+            pictureBox1.Invalidate();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (zoomFactor > zoomStep)
+            {
+                zoomFactor -= zoomStep;
+                pictureBox1.Invalidate(); 
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string url = "https://github.com/hoangmanhkhiem";
+            try
+            {
+                System.Diagnostics.Process.Start(new ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Không thể mở liên kết: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
